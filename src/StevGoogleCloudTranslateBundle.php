d25474682cb3572ae8ad81c63c1983b7d1ebcb28
Stefan Matei <stev.matei@gmail.com>
2022-04-13 10:39:09 +0300

    removed parent call

--- src/StevGoogleCloudTranslateBundle.php
+++ src/StevGoogleCloudTranslateBundle.php
@@ -16,7 +16,6 @@ class StevGoogleCloudTranslateBundle extends Bundle
 
     public function build(ContainerBuilder $container)
     {
-        parent::build($container);
 
         $container->addCompilerPass(new LoadersPass());
     }
